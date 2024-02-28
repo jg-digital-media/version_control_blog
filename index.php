89ed687cf4738aa47686b9cf82426aebf1f41f55
Jonathan Grieve <mail@jonniegrieve.co.uk>
2024-02-28 16:37:34 +0000

    starting point for blog

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <title>Version Control Blog</title>
+</head>
+
+<body>
+    
+
+    <h1>Temporary project - a use case into the use of version control</h1>
+    <hr />
+
+    <table>
+        <tr>
+            <th>Version</th>
+            <th>Details</th> 
+        </tr>
+        <tr>
+            <td>1.0</td>
+            <td>Sensors indicate no shuttle or other ships in this sector. Tractor beam released, sir.</td> 
+        </tr>
+        <tr>
+            <td>1.2</td>
+            <td>According to coordinates, we have travelled 7,000 light years and are located near the system J-25.</td>
+        </tr>
+        <tr>
+            <td>1.3</td>
+            <td>Without our shields, at this range it is probable a photon detonation could destroy the Enterprise.</td>
+        </tr>
+    </table>
+
+    <div class="future-projects">
+
+        <h2>Future Projects</h2>
+
+        <ul>
+            <li>Damage report? Sections 27, 28 and 29 on decks four, five and six destroyed.</li>
+            <li>Radiation levels in our atmosphere have increased by 3,000 percent. </li>
+            <li>We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies.</li>
+            <li>It indicates a synchronic distortion in the areas emanating triolic waves.</li>
+        </ul>
+
+    </div>
+
+    <!--
+        Repository - Setting Up
+        Add project from list add.
+        Create a branch - checkout between branches. Note the differences between the branchs
+        merge into main branch.  Push
+
+        Edit a log - Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. 
+
+        View the repository - clone it to reveal the log and list of branches
+
+
+     -->
+
+</body>
+</html>
